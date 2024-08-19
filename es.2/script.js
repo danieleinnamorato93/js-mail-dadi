2c83e6a689b4d0dbfece8682443620435f97dde1
danieleinnamorato93 <daniele.innamorato93@gmail.com>
2024-08-19 16:55:38 +0200

    authorized E mail added

--- es.2/script.js
+++ es.2/script.js
@@ -4,3 +4,10 @@ const emailAdd=document.getElementById('email');
 const logBtn=document.getElementById('log-btn');
 const notificationElement=document.getElementById('notification');
 console.log('email' , 'log-btn', 'notification')
+
+//Aggiungo le mail autorizzate
+const emailAuthorized = [
+    "daniele.innamorato@wow.com" ,
+    "giovanni.moretti@wow.com" ,
+    "luca.rossi@wow.com" ,
+];
