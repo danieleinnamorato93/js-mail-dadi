5754845bd0e4d0603254b9f446b3d15c6f06a098
danieleinnamorato93 <daniele.innamorato93@gmail.com>
2024-08-19 15:35:20 +0200

    risultato e vincitore aggiunti in console

--- es.1/script.js
+++ es.1/script.js
@@ -1,9 +1,21 @@
+//Preparazione
 console.log('JS OK')
 let result = null;
 
-
+//genero lanci
 const rollPlayer = Math.floor(Math.random() *20 ) +1 ;
 const rollCpu = Math.floor(Math.random() *20 ) +1 ;
 
+//mostro in console
 document.getElementById("player").innerHTML = `Lancio player 1: ${rollPlayer}`;
 document.getElementById("cpu").innerHTML = `Lancio Cpu: ${rollCpu}`;
+
+//Stabilisco il vincitore
+if(rollPlayer > rollCpu) {
+    result = "Congratulazioni, hai vinto!";
+} else if(rollPlayer < rollCpu) {
+    result = "Hai perso! Ritenta, sarai più fortunato!";
+} else {
+    result = "Pari!";
+}
+console.log('result' ,  result)
