fdd8e7ed268de8b38e0e20c34b3a9b96f96a74c1
danieleinnamorato93 <daniele.innamorato93@gmail.com>
2024-08-19 15:25:02 +0200

    Risultati in console

--- es.1/script.js
+++ es.1/script.js
@@ -5,3 +5,5 @@ let result = null;
 const rollPlayer = Math.floor(Math.random() *20 ) +1 ;
 const rollCpu = Math.floor(Math.random() *20 ) +1 ;
 
+document.getElementById("player").innerHTML = `Lancio player 1: ${rollPlayer}`;
+document.getElementById("cpu").innerHTML = `Lancio Cpu: ${rollCpu}`;
